9488b83717984d3d36fe062130c064fd79cb4034
Sam Christensen <samochristensen@gmail.com>
2024-08-08 16:56:57 -0600

    add uart functionality - include definitions.h inside app.c

--- src/app.c
+++ src/app.c
@@ -11,13 +11,13 @@
     This file contains the source code for the MPLAB Harmony application.
 
   Description:
-    This file contains the source code for the MPLAB Harmony application.  It
+    This file contains the source code for the MPLAB Harmony application. It
     implements the logic of the application's state machine and it may call
     API routines of other MPLAB Harmony modules in the system, such as drivers,
-    system services, and middleware.  However, it does not call any of the
+    system services, and middleware. However, it does not call any of the
     system interfaces (such as the "Initialize" and "Tasks" functions) of any of
     the modules in the system or make any assumptions about when those functions
-    are called.  That is the responsibility of the configuration-specific system
+    are called. That is the responsibility of the configuration-specific system
     files.
  *******************************************************************************/
 
@@ -28,6 +28,7 @@
 // *****************************************************************************
 
 #include "app.h"
+#include "definitions.h"
 
 // *****************************************************************************
 // *****************************************************************************
@@ -35,21 +36,18 @@
 // *****************************************************************************
 // *****************************************************************************
 
-// *****************************************************************************
+#define UART_BUFFER_SIZE 100
+
+// Buffer to hold received data
+uint8_t uartRxBuffer[UART_BUFFER_SIZE];
+
+// Flag to indicate UART data reception
+volatile bool uartDataReady = false;
+
 /* Application Data
-
-  Summary:
-    Holds application data
-
-  Description:
-    This structure holds the application's data.
-
-  Remarks:
-    This structure should be initialized by the APP_Initialize function.
-
-    Application strings and buffers are be defined outside this structure.
+   Holds application data
+   This structure should be initialized by the APP_Initialize function.
 */
-
 APP_DATA appData;
 
 // *****************************************************************************
@@ -58,19 +56,17 @@ APP_DATA appData;
 // *****************************************************************************
 // *****************************************************************************
 
-/* TODO:  Add any necessary callback functions.
-*/
+// UART read callback function
+void UART_ReadCallback(uintptr_t context)
+{
+    uartDataReady = true;
+}
 
 // *****************************************************************************
 // *****************************************************************************
 // Section: Application Local Functions
 // *****************************************************************************
 // *****************************************************************************
-
-
-/* TODO:  Add any necessary local functions.
-*/
-
 
 // *****************************************************************************
 // *****************************************************************************
@@ -91,13 +87,12 @@ void APP_Initialize ( void )
     /* Place the App state machine in its initial state. */
     appData.state = APP_STATE_INIT;
 
+    /* Register the UART read callback function */
+    SERCOM3_USART_ReadCallbackRegister(UART_ReadCallback, (uintptr_t)NULL);
 
-
-    /* TODO: Initialize your application's state machine and other
-     * parameters.
-     */
+    /* Start UART read operation */
+    SERCOM3_USART_Read(uartRxBuffer, sizeof(uartRxBuffer));
 }
-
 
 /******************************************************************************
   Function:
@@ -109,7 +104,6 @@ void APP_Initialize ( void )
 
 void APP_Tasks ( void )
 {
-
     /* Check the application's current state. */
     switch ( appData.state )
     {
@@ -118,9 +112,11 @@ void APP_Tasks ( void )
         {
             bool appInitialized = true;
 
-
             if (appInitialized)
             {
+                // Send a welcome message via UART
+                char message[] = "UART Communication Initialized.\r\n";
+                SERCOM3_USART_Write(message, sizeof(message) - 1);
 
                 appData.state = APP_STATE_SERVICE_TASKS;
             }
@@ -129,17 +125,24 @@ void APP_Tasks ( void )
 
         case APP_STATE_SERVICE_TASKS:
         {
+            // Check if UART data has been received
+            if (uartDataReady)
+            {
+                uartDataReady = false;
 
+                // Echo received data back
+                SERCOM3_USART_Write(uartRxBuffer, sizeof(uartRxBuffer));
+
+                // Reinitiate UART read
+                SERCOM3_USART_Read(uartRxBuffer, sizeof(uartRxBuffer));
+            }
             break;
         }
-
-        /* TODO: implement your application state machine.*/
-
 
         /* The default state should never be executed. */
         default:
         {
-            /* TODO: Handle error in application's state machine. */
+            /* Handle error in application's state machine. */
             break;
         }
     }
